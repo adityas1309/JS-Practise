8509f7be050c16b076ba5a18b2411b21c568e21a
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-02 00:00:00 +0530

    Added JavaScript practice question - Day 2

--- js-practice/day2.js
+++ js-practice/day2.js
@@ -14,3 +14,13 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString('hello')); // 'olleh'
+
+// Question: 6. Check prime number
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+console.log(isPrime(7)); // true

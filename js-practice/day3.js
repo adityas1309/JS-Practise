149bd78f3adbc121bcef32f3907fd64164474c58
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

--- js-practice/day3.js
+++ js-practice/day3.js
@@ -7,3 +7,9 @@ function celsiusToFahrenheit(c) {
     return (c * 9/5) + 32;
 }
 console.log(celsiusToFahrenheit(25)); // 77
+
+// Question: 10. Find array maximum
+function findArrayMax(arr) {
+    return Math.max(...arr);
+}
+console.log(findArrayMax([5, 2, 9, 1])); // 9

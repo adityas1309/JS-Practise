3c629b8715bb3cbde6c785c50bb67ba121ca5d75
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-03 00:00:00 +0530

    Added JavaScript practice question - Day 3

---
+++ js-practice/day3.js
@@ -0,0 +1,9 @@
+
+// Question: 11. Celsius to Fahrenheit conversion
+
+// Conversion formula implementation
+function celsiusToFahrenheit(c) {
+    // (C × 9/5) + 32
+    return (c * 9/5) + 32;
+}
+console.log(celsiusToFahrenheit(25)); // 77

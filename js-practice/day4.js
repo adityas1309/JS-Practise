ba0056398908e368a8c1ed6b3dc1930ea62d6e8e
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-04 00:00:00 +0530

    Added JavaScript practice question - Day 4

--- js-practice/day4.js
+++ js-practice/day4.js
@@ -7,3 +7,12 @@ function checkStringLength(str, max) {
     return str.length <= max;
 }
 console.log(checkStringLength('hello', 5)); // true
+
+// Question: 18. Calculate rectangle area
+
+// Basic geometry calculation
+function rectangleArea(width, height) {
+    // Multiply dimensions
+    return width * height;
+}
+console.log(rectangleArea(5, 4)); // 20

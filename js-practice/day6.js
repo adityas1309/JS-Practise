8b93a41c42ad6e2759a6023b46656cd58d25ee8a
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -40,3 +40,14 @@ function sumDigits(num) {
     );
 }
 console.log(sumDigits(123)); // 6
+
+// Question: 27. Capitalize first letter
+
+// Capitalize first character
+function capitalize(str) {
+    // Handle empty string case
+    return str.length > 0 
+        ? str[0].toUpperCase() + str.slice(1) 
+        : '';
+}
+console.log(capitalize('test')); // 'Test'

27d2cc8d06d0128bcb2bd313d5531ed1726c02ba
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -7,3 +7,17 @@ function mergeArrays(arr1, arr2) {
     return [...new Set([...arr1, ...arr2])];
 }
 console.log(mergeArrays([1,2], [2,3])); // [1,2,3]
+
+// Question: 31. FizzBuzz implementation
+
+// Classic FizzBuzz logic with modulus
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let output = '';
+        // Check divisibility rules
+        if (i % 3 === 0) output += 'Fizz';
+        if (i % 5 === 0) output += 'Buzz';
+        console.log(output || i); // Default to number if empty
+    }
+}
+fizzBuzz(5); // 1, 2, Fizz, 4, Buzz

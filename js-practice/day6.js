38c75599ec606b8b2848ab57c86fc7b8b90f61be
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-06 00:00:00 +0530

    Added JavaScript practice question - Day 6

--- js-practice/day6.js
+++ js-practice/day6.js
@@ -51,3 +51,16 @@ function capitalize(str) {
         : '';
 }
 console.log(capitalize('test')); // 'Test'
+
+// Question: 28. Reverse array elements
+
+// Manual array reversal
+function reverseArray(arr) {
+    const reversed = [];
+    // Iterate from end to start
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+console.log(reverseArray([1,2,3])); // [3,2,1]

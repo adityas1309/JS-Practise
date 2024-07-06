fa5b85b145e79d110c1d225d2056eaef1bcc7477
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-07 00:00:00 +0530

    Added JavaScript practice question - Day 7

--- js-practice/day7.js
+++ js-practice/day7.js
@@ -25,3 +25,12 @@ function arrayToString(arr, separator = ', ') {
     return arr.join(separator);
 }
 console.log(arrayToString(['a', 'b', 'c'], '-')); // 'a-b-c'
+
+// Question: 33. Check if object is empty
+
+// Check object key count
+function isEmpty(obj) {
+    // Convert keys to array and check length
+    return Object.keys(obj).length === 0;
+}
+console.log(isEmpty({})); // true

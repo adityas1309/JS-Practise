a90e22b748c53a915dcb5d9414917259926d2e20
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -10,3 +10,12 @@ function countOccurrences(arr) {
     }, {});
 }
 console.log(countOccurrences(['a','b','a','c'])); // {a:2, b:1, c:1}
+
+// Question: 36. Convert string to array of words
+
+// Split on whitespace characters
+function stringToWordArray(str) {
+    // Use split with regex for multiple spaces
+    return str.split(/\s+/);
+}
+console.log(stringToWordArray('Hello world JS')); // ['Hello', 'world', 'JS']

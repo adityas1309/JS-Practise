7bc48b4726f8f849548d78efcd1d3e7daafd9b38
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-08 00:00:00 +0530

    Added JavaScript practice question - Day 8

--- js-practice/day8.js
+++ js-practice/day8.js
@@ -28,3 +28,15 @@ function filterEvens(numbers) {
     return numbers.filter(num => num % 2 === 0);
 }
 console.log(filterEvens([1, 2, 3, 4])); // [2, 4]
+
+// Question: 39. Find index of element in array
+
+// Custom indexOf implementation
+function findIndex(arr, element) {
+    // Loop through array elements
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) return i;
+    }
+    return -1;
+}
+console.log(findIndex(['a', 'b', 'c'], 'b')); // 1

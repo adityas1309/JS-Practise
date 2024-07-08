7de33d06a93e1718ef928e7d3bb106af675afd86
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -18,3 +18,14 @@ function flattenArray(arr) {
     }, []);
 }
 console.log(flattenArray([1,[2,[3]]])); // [1,2,3]
+
+// Question: 44. Capitalize each word
+
+// Split and capitalize first letters
+function capitalizeWords(str) {
+    return str.split(' ').map(word => 
+        // Capitalize first letter + rest lowercase
+        word[0].toUpperCase() + word.slice(1).toLowerCase()
+    ).join(' ');
+}
+console.log(capitalizeWords('hello world')); // 'Hello World'

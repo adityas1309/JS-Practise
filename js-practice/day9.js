37a430bcab5c19d5a9666b7691220b182971335e
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-09 00:00:00 +0530

    Added JavaScript practice question - Day 9

--- js-practice/day9.js
+++ js-practice/day9.js
@@ -48,3 +48,12 @@ function isInteger(num) {
     return num === Math.floor(num);
 }
 console.log(isInteger(5.0)); // true
+
+// Question: 46. Random number in range
+
+// Generate random integer between min-max
+function randomInRange(min, max) {
+    // Scale and shift Math.random output
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(randomInRange(5, 10)); // Random integer 5-10

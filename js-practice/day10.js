fc6662d3ebf9e1b773fc00f92223daef872366b7
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-10 00:00:00 +0530

    Added JavaScript practice question - Day 10

--- js-practice/day10.js
+++ js-practice/day10.js
@@ -9,3 +9,12 @@ function longestWord(str) {
     , '');
 }
 console.log(longestWord('The quick brown fox')); // 'quick'
+
+// Question: 51. Sort array of numbers
+
+// Ascending sort with compare function
+function sortNumbers(arr) {
+    // Numeric sort using subtraction
+    return arr.sort((a, b) => a - b);
+}
+console.log(sortNumbers([3, 1, 4, 2])); // [1, 2, 3, 4]

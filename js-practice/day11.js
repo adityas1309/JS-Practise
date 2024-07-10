b42aebc3a4e718e2099f6eb113e7ad17041f172f
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-11 00:00:00 +0530

    Added JavaScript practice question - Day 11

--- js-practice/day11.js
+++ js-practice/day11.js
@@ -13,3 +13,13 @@ function generateUUID() {
     );
 }
 console.log(generateUUID()); // Random UUID v4
+
+// Question: 52. Sum of squares
+
+// Square and sum using reduce
+function sumOfSquares(numbers) {
+    return numbers.reduce((sum, num) => 
+        sum + (num * num), 0
+    );
+}
+console.log(sumOfSquares([2, 3])); // 13

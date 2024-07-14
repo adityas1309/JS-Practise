89d16d90dad58720ff76837a1d7bd5e688be7ffa
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-15 00:00:00 +0530

    Added JavaScript practice question - Day 15

--- js-practice/day15.js
+++ js-practice/day15.js
@@ -12,3 +12,25 @@ function flattenIterative(arr) {
     }
     return result;
 }
+
+// Question: 14. Event Emitter class
+```javascript
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        (this.events[event] || (this.events[event] = [])).push(listener);
+        return () => this.off(event, listener);
+    }
+
+    off(event, listener) {
+        if (!this.events[event]) return;
+        this.events[event] = this.events[event].filter(fn => fn !== listener);
+    }
+
+    emit(event, ...args) {
+        (this.events[event] || []).forEach(fn => fn(...args));
+    }
+}

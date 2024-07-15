cd9716854c7ebfb375ae3b0bd99d517423139fa6
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-16 00:00:00 +0530

    Added JavaScript practice question - Day 16

--- js-practice/day16.js
+++ js-practice/day16.js
@@ -12,3 +12,29 @@ function allSettled(promises) {
 
 // Usage:
 allSettled([Promise.resolve(1), Promise.reject('error')]);
+
+// Question: 20. Scheduler with delayed tasks
+```javascript
+class Scheduler {
+    constructor() {
+        this.queue = [];
+        this.isProcessing = false;
+    }
+
+    add(task, delay) {
+        return new Promise(resolve => {
+            this.queue.push({ task, delay, resolve });
+            if (!this.isProcessing) this.process();
+        });
+    }
+
+    async process() {
+        this.isProcessing = true;
+        while (this.queue.length) {
+            const { task, delay, resolve } = this.queue.shift();
+            await new Promise(r => setTimeout(r, delay));
+            resolve(await task());
+        }
+        this.isProcessing = false;
+    }
+}

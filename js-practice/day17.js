0011ba77409705bf06beac37e81dd0960ffd9909
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-17 00:00:00 +0530

    Added JavaScript practice question - Day 17

--- js-practice/day17.js
+++ js-practice/day17.js
@@ -34,3 +34,28 @@ const middlewares = [
     val => val * 2,
     val => new Promise(r => setTimeout(() => r(val + 3), 100))
 ];
+
+// Question: 26. Circular buffer implementation
+```javascript
+class CircularBuffer {
+    constructor(capacity) {
+        this.buffer = new Array(capacity);
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    push(item) {
+        this.buffer[this.tail] = item;
+        this.tail = (this.tail + 1) % this.buffer.length;
+        this.size = Math.min(this.size + 1, this.buffer.length);
+    }
+
+    pop() {
+        if (this.size === 0) return null;
+        const item = this.buffer[this.head];
+        this.head = (this.head + 1) % this.buffer.length;
+        this.size--;
+        return item;
+    }
+}

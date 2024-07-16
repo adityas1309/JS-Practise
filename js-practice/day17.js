4a95c75797c0779f0e7dd5108ff85762390b48e2
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-17 00:00:00 +0530

    Added JavaScript practice question - Day 17

--- js-practice/day17.js
+++ js-practice/day17.js
@@ -19,3 +19,18 @@ function bfs(graph, start) {
 
 // Adjacency list format:
 const graph = { A: ['B', 'C'], B: ['D'], C: [], D: [] };
+
+// Question: 28. Async middleware pipeline
+```javascript
+async function pipeline(middlewares, initial) {
+    return middlewares.reduce(
+        (acc, middleware) => acc.then(val => middleware(val)),
+        Promise.resolve(initial)
+    );
+}
+
+// Usage:
+const middlewares = [
+    val => val * 2,
+    val => new Promise(r => setTimeout(() => r(val + 3), 100))
+];

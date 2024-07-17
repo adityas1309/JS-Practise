c112e6213e38d8bf007847c2249d4afa88632522
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-18 00:00:00 +0530

    Added JavaScript practice question - Day 18

--- js-practice/day18.js
+++ js-practice/day18.js
@@ -76,3 +76,33 @@ class ImmutableList {
         return [...this._data];
     }
 }
+
+// Question: 32. Reactive streams with operators
+```javascript
+class ObservableStream {
+    constructor(source) {
+        this.subscribers = [];
+        source(this._emit.bind(this));
+    }
+
+    _emit(value) {
+        this.subscribers.forEach(fn => fn(value));
+    }
+
+    pipe(...operators) {
+        // Chain operators sequentially
+        return operators.reduce((obs, op) => op(obs), this);
+    }
+
+    subscribe(fn) {
+        this.subscribers.push(fn);
+        return () => {
+            this.subscribers = this.subscribers.filter(f => f !== fn);
+        };
+    }
+}
+
+// Operator example:
+const map = f => source => new ObservableStream(emit => {
+    source.subscribe(val => emit(f(val)));
+};

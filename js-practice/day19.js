fbabe2a66ca8ae98b23aec676b0064a666edeacc
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-19 00:00:00 +0530

    Added JavaScript practice question - Day 19

---
+++ js-practice/day19.js
@@ -0,0 +1,28 @@
+
+// Question: 39. Memento pattern for undo/redo
+```javascript
+class HistoryManager {
+    constructor() {
+        this.states = [];
+        this.position = -1;
+    }
+
+    push(state) {
+        // Clear redo states when pushing new state
+        this.states.splice(this.position + 1);
+        this.states.push(JSON.stringify(state));
+        this.position = this.states.length - 1;
+    }
+
+    undo() {
+        return this.position > 0 
+            ? JSON.parse(this.states[--this.position])
+            : null;
+    }
+
+    redo() {
+        return this.position < this.states.length - 1 
+            ? JSON.parse(this.states[++this.position])
+            : null;
+    }
+}

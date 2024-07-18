bc8619a5bde3307a5e571cc7c3ed9e00680911c2
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-19 00:00:00 +0530

    Added JavaScript practice question - Day 19

--- js-practice/day19.js
+++ js-practice/day19.js
@@ -70,3 +70,29 @@ async function batchPromises(items, processFn, batchSize = 10) {
 
 // Usage:
 batchPromises(urls, fetchUrl, 5);
+
+// Question: 38. Observable async iterator
+```javascript
+function createAsyncIterable() {
+    const queue = [];
+    let resolveNext;
+    
+    return {
+        async *[Symbol.asyncIterator]() {
+            while (true) {
+                if (queue.length) yield queue.shift();
+                else await new Promise(r => resolveNext = r);
+            }
+        },
+        push(value) {
+            queue.push(value);
+            if (resolveNext) {
+                resolveNext();
+                resolveNext = null;
+            }
+        }
+    };
+}
+
+// Usage:
+const asyncIterable = createAsyncIterable();

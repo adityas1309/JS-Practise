e7d56fc75d5326fa0df1e29e044d2f2a4e313c88
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-20 00:00:00 +0530

    Added JavaScript practice question - Day 20

--- js-practice/day20.js
+++ js-practice/day20.js
@@ -61,3 +61,36 @@ function weakMemoize(fn) {
 const memoizedObjProcessor = weakMemoize(obj => {
     return Object.keys(obj).join(',');
 });
+
+// Question: 41. Custom async iterator
+```javascript
+class AsyncRange {
+    constructor(start, end, delay = 100) {
+        this.start = start;
+        this.end = end;
+        this.delay = delay;
+    }
+
+    [Symbol.asyncIterator]() {
+        return {
+            current: this.start,
+            last: this.end,
+            async next() {
+                await new Promise(resolve => 
+                    setTimeout(resolve, this.delay)
+                );
+                
+                return this.current <= this.last 
+                    ? { done: false, value: this.current++ }
+                    : { done: true };
+            }
+        };
+    }
+}
+
+// Usage:
+(async () => {
+    for await (let num of new AsyncRange(1, 5)) {
+        console.log(num); // Logs 1-5 with 100ms delay
+    }
+})();

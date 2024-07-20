fa647bd41a14ac727242a91dc94deca0ee102a13
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-21 00:00:00 +0530

    Added JavaScript practice question - Day 21

--- js-practice/day21.js
+++ js-practice/day21.js
@@ -60,3 +60,31 @@ class PriorityPromiseQueue {
         }
     }
 }
+
+// Question: 49. Command pattern with undo
+```javascript
+class CommandManager {
+    constructor() {
+        this.history = [];
+        this.position = -1;
+    }
+
+    execute(command) {
+        command.execute();
+        this.history = this.history.slice(0, this.position + 1);
+        this.history.push(command);
+        this.position++;
+    }
+
+    undo() {
+        if (this.position >= 0) {
+            this.history[this.position--].undo();
+        }
+    }
+
+    redo() {
+        if (this.position < this.history.length - 1) {
+            this.history[++this.position].execute();
+        }
+    }
+}

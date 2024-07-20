71d04ca67c3f1f4feca96e41307c5610e66e1f10
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-21 00:00:00 +0530

    Added JavaScript practice question - Day 21

--- js-practice/day21.js
+++ js-practice/day21.js
@@ -19,3 +19,24 @@ class ImageProxy {
 // Usage:
 const proxy = new ImageProxy('large-image.jpg');
 document.body.appendChild(proxy.load());
+
+// Question: 48. Observable properties
+```javascript
+function observe(obj, prop, callback) {
+    let value = obj[prop];
+    
+    Object.defineProperty(obj, prop, {
+        get() { return value; },
+        set(newVal) {
+            const oldVal = value;
+            value = newVal;
+            callback(newVal, oldVal);
+        }
+    });
+}
+
+// Usage:
+const user = { name: 'Alice' };
+observe(user, 'name', (newVal, oldVal) => {
+    console.log(`Name changed from ${oldVal} to ${newVal}`);
+});

1e3f944ada6dd791a2ab92773416aa9542d66ef0
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-22 00:00:00 +0530

    Added JavaScript practice question - Day 22

--- js-practice/day22.js
+++ js-practice/day22.js
@@ -19,3 +19,30 @@ class WSReconnect {
         };
     }
 }
+
+// Question: 52. Tree data structure implementation
+```javascript
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+
+    addChild(node) {
+        this.children.push(node);
+    }
+
+    traverseDFS(cb) {
+        cb(this.value);
+        this.children.forEach(child => child.traverseDFS(cb));
+    }
+
+    traverseBFS(cb) {
+        const queue = [this];
+        while (queue.length) {
+            const node = queue.shift();
+            cb(node.value);
+            queue.push(...node.children);
+        }
+    }
+}

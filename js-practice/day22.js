55ce4c976823bdd0ec3c3180bb048e4493eb879c
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-22 00:00:00 +0530

    Added JavaScript practice question - Day 22

--- js-practice/day22.js
+++ js-practice/day22.js
@@ -74,3 +74,30 @@ class ConcurrentQueue {
         }
     }
 }
+
+// Question: 50. Lazy evaluator
+```javascript
+class Lazy {
+    constructor() {
+        this.chain = [];
+    }
+
+    add(fn, ...args) {
+        this.chain.push(items => fn(...args, items));
+        return this;
+    }
+
+    eval(target) {
+        return this.chain.reduce(
+            (acc, fn) => fn(acc),
+            target
+        );
+    }
+}
+
+// Usage:
+const processor = new Lazy()
+    .add(arr => arr.map(x => x * 2))
+    .add(arr => arr.filter(x => x > 5));
+
+processor.eval([1, 2, 3]); // [6]

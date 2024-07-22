961846232dd6038d1cd6ae9af1e4545166e36a87
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-23 00:00:00 +0530

    Added JavaScript practice question - Day 23

--- js-practice/day23.js
+++ js-practice/day23.js
@@ -67,3 +67,36 @@ class AsyncMutex {
         }
     }
 }
+
+// Question: 56. Dynamic promise pool
+```javascript
+class PromisePool {
+    constructor(initialConcurrency = 4) {
+        this.concurrency = initialConcurrency;
+        this.queue = [];
+        this.active = 0;
+    }
+
+    add(task) {
+        return new Promise((resolve, reject) => {
+            this.queue.push({ task, resolve, reject });
+            this.run();
+        });
+    }
+
+    run() {
+        while (this.active < this.concurrency && this.queue.length) {
+            const { task, resolve, reject } = this.queue.shift();
+            this.active++;
+            task().then(resolve, reject).finally(() => {
+                this.active--;
+                this.run();
+            });
+        }
+    }
+
+    setConcurrency(newConcurrency) {
+        this.concurrency = newConcurrency;
+        this.run();
+    }
+}

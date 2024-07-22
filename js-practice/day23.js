05af95543ca35d39fd54a532aa6f2c8458887955
adityasinghparmar13 <142864215+adityas1309@users.noreply.github.com>
2024-07-23 00:00:00 +0530

    Added JavaScript practice question - Day 23

--- js-practice/day23.js
+++ js-practice/day23.js
@@ -100,3 +100,23 @@ class PromisePool {
         this.run();
     }
 }
+
+// Question: 60. Priority event dispatcher
+```javascript
+class PriorityEventDispatcher {
+    constructor() {
+        this.listeners = new Map();
+    }
+
+    on(event, handler, priority = 0) {
+        const handlers = this.listeners.get(event) || [];
+        handlers.push({ handler, priority });
+        handlers.sort((a, b) => b.priority - a.priority);
+        this.listeners.set(event, handlers);
+    }
+
+    emit(event, ...args) {
+        const handlers = this.listeners.get(event) || [];
+        handlers.forEach(({ handler }) => handler(...args));
+    }
+}
